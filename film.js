24aee8baa8b233c29efc96e7b0a680602e0f1f52
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-31 23:35:28 +0300

    hw-4-d1

---
+++ film.js
@@ -0,0 +1,18 @@
+const filmList = document.querySelector(".film-list")
+axios.get("https://ghibliapi.herokuapp.com/films")
+.then(function(response){
+    const responseData = response.data
+    console.log ("responseData:",responseData)
+    responseData.forEach(function(data)  {
+        const title=data.title
+        const image = data.image 
+        const release_date=data.release_data 
+const films =`
+<div class="films">
+<h2> films names is : ${title} <br> the release_date is : ${release_date}</h2>
+<img src ="${image}" height ="200px">
+</div>
+`
+filmList.innerHTML+=films
+    });
+})
